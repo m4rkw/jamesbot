2eb5819801a6efde61eecf03adc1f25137d6a2d8
Mark Wadham <m@rkw.io>
2018-03-29 09:03:25 +0100

    update

--- jamesbot_api.rb
+++ jamesbot_api.rb
@@ -7,9 +7,7 @@ class JamesBot
       69 => "shut up, i'm perpetrating lisp macros.",
       70 => "fucking hipster",
       100 => "that sounds shit.",
-      150 => "either i work from home or your entire company fails.",
       102 => "i want people to use scala because i can laugh when they fail",
-      103 => "OutOfMemoryError on the left, thread limit on the right, here i am stuck in the middle with the pieces of my dreams and proge",
       104 => "what it is, it's composed of tedium",
       106 => "can i do it from home?",
       107 => "yeah well, you would think that.",
@@ -21,10 +19,10 @@ class JamesBot
       151 => "you think war is bad, try c++",
       152 => "that's the sort of thing that brings back enthusiasm to drink",
       154 => "fascinating.",
-      155 => "seriously if i have to google for you to make you shut up i will be most displeased",
       160 => "use c++",
       161 => "dude where's my car?",
-      162 => "duuude... what if... what if clojure is sativa and common lisp is.. indica!"
+      163 => "that's what she said",
+      164 => "inorite"
     }
   end
 
